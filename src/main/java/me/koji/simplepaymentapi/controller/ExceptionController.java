a2b7a74f366360f6233a534fdb27b2cf6d2d4739
Koji <ksobrino.comercial@gmail.com>
2025-10-25 19:37:48 -0300

    FEATURE: Added a new exception treatment.

--- src/main/java/me/koji/simplepaymentapi/controller/ExceptionController.java
+++ src/main/java/me/koji/simplepaymentapi/controller/ExceptionController.java
@@ -1,15 +1,20 @@
 package me.koji.simplepaymentapi.controller;
 
 import me.koji.simplepaymentapi.dto.ServerInternalErrorDTO;
+import me.koji.simplepaymentapi.exceptions.InvalidUserException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionController {
-
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ServerInternalErrorDTO> handleException(IllegalArgumentException exception) {
+    public ResponseEntity<ServerInternalErrorDTO> handleIllegalArgument(IllegalArgumentException exception) {
+        return ResponseEntity.badRequest().body(new ServerInternalErrorDTO(exception.getMessage(), 400));
+    }
+
+    @ExceptionHandler(InvalidUserException.class)
+    public ResponseEntity<ServerInternalErrorDTO> handleInvalidUser(InvalidUserException exception) {
         return ResponseEntity.internalServerError().body(new ServerInternalErrorDTO(exception.getMessage(), 500));
     }
 }
