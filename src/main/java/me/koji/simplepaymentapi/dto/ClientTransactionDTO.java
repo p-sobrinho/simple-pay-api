49e882ec48653e10b4f5a55f246b028ad435b972
Koji <ksobrino.comercial@gmail.com>
2025-10-25 21:59:40 -0300

    FEATURE: Added new validation via annotations.

--- src/main/java/me/koji/simplepaymentapi/dto/ClientTransactionDTO.java
+++ src/main/java/me/koji/simplepaymentapi/dto/ClientTransactionDTO.java
@@ -1,9 +1,19 @@
 package me.koji.simplepaymentapi.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
 public record ClientTransactionDTO(
-        Long id, Long sender, Long receiver,
-        String message, BigDecimal value, Instant timestamp
+        Long id,
+        @NotNull(message = "Sender is required.")
+        Long sender,
+        @NotNull(message = "Receiver is required.")
+        Long receiver,
+        String message,
+        @NotNull(message = "Value is required.")
+        BigDecimal value,
+        Instant timestamp
 ) { }
--- src/main/java/me/koji/simplepaymentapi/dto/ClientUserDTO.java
+++ src/main/java/me/koji/simplepaymentapi/dto/ClientUserDTO.java
@@ -1,11 +1,25 @@
 package me.koji.simplepaymentapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import me.koji.simplepaymentapi.types.ClientUserType;
 
 import java.math.BigDecimal;
 
 public record ClientUserDTO(
-        Long id, String firstName, String lastName,
-        String email, String cpf, String password,
+        Long id,
+        @NotBlank(message = "First name is required.")
+        String firstName,
+        @NotBlank(message = "Last name is required.")
+        String lastName,
+        @Email(message = "Email must be valid.")
+        @NotBlank(message = "Email is required.")
+        String email,
+        @Pattern(regexp = "\\d{11}", message = "CPF must have 11 digits.")
+        String cpf,
+        @Size(min = 8, message = "Password size must be equals or greater than 8.")
+        String password,
         BigDecimal balance, ClientUserType type
 ){ }
